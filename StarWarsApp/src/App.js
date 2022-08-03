726e2293ec699a2ebf119798a6d3f35b9f9fe83b
isyoo1213 <moethns@gmail.com>
2022-08-03 15:40:28 +0900

    feat: Add ErrorHandling

--- StarWarsApp/src/App.js
+++ StarWarsApp/src/App.js
@@ -6,20 +6,36 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setError(null);
+    {/* 새로운 요청을 보낼 때 이전에 설정된 error에 대한 초기화처리에 필요 
+        async - try catch/ promise - cache*/}
+
+    try{
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      if(!response.ok){
+        throw new Error('An Error accured!')
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch(error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
 
@@ -30,8 +46,9 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found No Movies.</p>}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found No Movies.</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
