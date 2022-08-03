c62beb6ef1f1b353ef1debd69788ba9d05879819
isyoo1213 <moethns@gmail.com>
2022-08-03 14:17:07 +0900

    feat: Change then to async await

--- StarWarsApp/src/components/Movie.js
+++ StarWarsApp/src/components/Movie.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import classes from './Movie.module.css';
+import styles from './Movie.module.css';
 
 const Movie = (props) => {
   return (
-    <li className={classes.movie}>
+    <li className={styles.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
