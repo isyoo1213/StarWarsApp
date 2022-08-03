af361580aa137daf7b96ab6217456ded51e8f274
isyoo1213 <moethns@gmail.com>
2022-08-03 19:36:47 +0900

    feat: Add logic for fetching data brought from fire-base

--- StarWarsApp/src/App.js
+++ StarWarsApp/src/App.js
@@ -8,65 +8,101 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  
-  const fetchMoviesHandler = useCallback( async () => {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    {/* 새로운 요청을 보낼 때 이전에 설정된 error에 대한 초기화처리에 필요 
-        async - try catch/ promise - cache*/}
+    {
+      /* 새로운 요청을 보낼 때 이전에 설정된 error에 대한 초기화처리에 필요 
+        async - try catch/ promise - cache*/
+    }
 
-    try{
-      const response = await fetch("https://starwarsmovieapp-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json");
+    try {
+      const response = await fetch(
+        "https://starwarsmovieapp-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+      );
 
-      if(!response.ok){
-        throw new Error('An Error accured!')
+      if (!response.ok) {
+        throw new Error("An Error accured!");
       }
 
       const data = await response.json();
+      console.log(data);
+      {
+        /* firebase에서 전송받은 객체에 암호화된 ID부여해 객체생성 + key또한 암호화된 값으로 설정하고 실제 데이터는 이 key에 대한 값으로 객체형태로 이루어짐 */
+      }
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({ 
+          id: key, 
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate 
+        });
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      // setMovies(transformedMovies);
+      
+      setMovies(loadedMovies);
       setIsLoading(false);
-    } catch(error) {
+    } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
   }, []);
-  {/* state의 set함수는 리액트 자체적으로 변하지 않는 함수로 인식하므로 의존성에 추가할 필요 없음
-      + 외부에서 사용하는 변수가 없으므로 의존성 추가할 필요 없음 */}
-
-  useEffect(()=> {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler])
-  {/* effect를 핸들러보다 먼저 호출할 시 hoisting 문제?로 핸들러가 데이터를 파싱하기 전에 핸들러 함수를 호출하므로 오류발생 */}
-
-  const addMovieHandler = (movie) => {
-    console.log(movie);
+  {
+    /* state의 set함수는 리액트 자체적으로 변하지 않는 함수로 인식하므로 의존성에 추가할 필요 없음
+      + 외부에서 사용하는 변수가 없으므로 의존성 추가할 필요 없음 */
   }
 
-  let content = <p>Found no movies</p>
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  {
+    /* effect를 핸들러보다 먼저 호출할 시 hoisting 문제?로 핸들러가 데이터를 파싱하기 전에 핸들러 함수를 호출하므로 오류발생 */
+  }
 
-  if(isLoading){
-    content = <p>Loading...</p>
-  } else if (error){
-    content = <p>{error}</p>
-  } else if(movies.length === 0 || movies.length < 0) {
-    content = <p>Found No Movies.</p>
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://starwarsmovieapp-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  let content = <p>Found no movies</p>;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (error) {
+    content = <p>{error}</p>;
+  } else if (movies.length === 0 || movies.length < 0) {
+    content = <p>Found No Movies.</p>;
   } else {
-    content = <MoviesList movies={movies} />
+    content = <MoviesList movies={movies} />;
   }
 
   return (
     <React.Fragment>
       <section>
-        <AddMovie onAddMovie={addMovieHandler}/>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
